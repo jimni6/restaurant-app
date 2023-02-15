86f23f8e78be4a8e3c70d7593b39cbb1b67da672
NiJimmy <nijimmy@outlook.fr>
2023-02-15 14:26:42 +0100

    Font changed for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Pacifico } from '@next/font/google';
+
+const pacifico = Pacifico({ 
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={pacifico.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
