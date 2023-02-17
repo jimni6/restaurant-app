d0d99d37ca3aa39ded1e9aff3b96e5254945cf21
NiJimmy <nijimmy@outlook.fr>
2023-02-17 16:11:20 +0100

    Handle revert lib 'react-reveal'

--- components/article.js
+++ components/article.js
@@ -1,28 +1,22 @@
 import Image from 'next/image';
 import styles from './article.module.css';
 import image from '../public/terrasse.webp';
-import Fade from 'react-reveal/Fade';
-
 
 export default function Article() {
     return (
         <>
             <div className={styles.container}>
-                <Fade left>
                     <Image
                         className={styles.image}
                         src={image}
                         alt='Terrasse du Little Cocotte'
                     />
-                </Fade>
-                <Fade right>
                     <div className={styles.text}>
                         <h2>Titre de larticle</h2>
                         <p>
                         Quand un jardinier et un cuisinier se rencontrent, de quoi parlent-ils ? De leur amour commun pour les légumes, bien sûr. Alain Baraton, jardinier en chef de Trianon et du Grand parc de Versailles, et Alain Ducasse ont noué une relation exceptionnelle qui permet aux clients du restaurant de savourer des légumes et des fruits cultivés exclusivement pour eux. Récoltés le matin, ils seront dans la cuisine quelques heures plus tard.
                         </p>
                     </div>
-                </Fade>
             </div>
         </>
     )
